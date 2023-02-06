7b0819623a7e82d30618d0b8a50b409d6eba8a89
Cheever Esler <ames.esler@gmail.com>
2023-02-06 11:24:19 -0400

    pratice with arrays

---
+++ 01-intro/arrays.js
@@ -0,0 +1,20 @@
+"use strict";
+// setting it up like this will give us type never[]!
+// const superHeroes: [] = [];
+exports.__esModule = true;
+// superHeroes.push("spiderman");
+// proper syntax:
+var superHeroes = [];
+// const heroPower: number[] = [];
+// alt syntax:
+var heroPower = [];
+// can set as any type, even custom
+var allUsers = [];
+// declaring array of arrays?
+var MLModels = [
+    [255, 255, 255],
+    [45]
+];
+superHeroes.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
---
+++ 01-intro/arrays.ts
@@ -0,0 +1,31 @@
+// setting it up like this will give us type never[]!
+// const superHeroes: [] = [];
+
+// superHeroes.push("spiderman");
+
+// proper syntax:
+const superHeroes: string[] = [];
+// const heroPower: number[] = [];
+// alt syntax:
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+}
+
+// can set as any type, even custom
+const allUsers: User[] = [];
+
+// declaring array of arrays?
+const MLModels: number[][] = [
+  [255, 255, 255],
+  [45]
+]
+
+superHeroes.push("spiderman");
+heroPower.push(2);
+
+allUsers.push({name: "", isActive: true});
+
+export {}
